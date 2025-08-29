9bbf7213de4dac3a9a42340d51d5289623e16a94
jungbini <jungbini@gmail.com>
2025-08-29 16:30:22 +0900

    Fix: Rename `_camera` to `_cameras` for clarity

--- lib/week04/video_call/camera_example.dart
+++ lib/week04/video_call/camera_example.dart
@@ -1,7 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-late List<CameraDescription> _camera;
+late List<CameraDescription> _cameras;
 
 Future<void> main() async {
   
